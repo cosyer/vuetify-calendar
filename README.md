26273faa402bb9d4dfca2738b37d8f810c6c12ec
cosyer <chenyu@mydearest.cn>
2020-07-30 01:26:47 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# canlendar-vuetify
+
+基于[dayspan-vuetify](https://github.com/ClickerMonkey/dayspan-vuetify)的demo
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import DaySpanVuetify from "dayspan-vuetify";
+import App from "./App.vue";
+import zh from './zh-CN';
+
+import "vuetify/dist/vuetify.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "dayspan-vuetify/dist/lib/dayspan-vuetify.min.css";
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
+
+Vue.use(DaySpanVuetify, {
+  methods: {
+    getDefaultEventColor: () => "#1976d2",
+  },
+});
+
+Vue.$dayspan.addLocales(['zh', 'zh-CN'], zh);
+Vue.$dayspan.setLocale('zh', true)
+
+new Vue({
+  el: "#app",
+  render: (h) => h(App),
+});
